b2918d88a4ea3d3cd67128c027c47ec129440a40
Janne Aikioniemi <janne.aikioniemi@siili.com>
2019-05-22 11:56:27 +0300

    Adds setting the main photo functionality

--- Controllers/PhotosController.cs
+++ Controllers/PhotosController.cs
@@ -58,7 +58,7 @@ namespace DatingApp.API.Controllers
 
             if (userId != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
                 return Unauthorized();
-                
+
             var userFromRepo = await _repo.GetUser(userId);
             var uploadResult = new ImageUploadResult();
 
@@ -95,6 +95,34 @@ namespace DatingApp.API.Controllers
             }
 
             return BadRequest("Could not add the photo");
+        }
+
+        // Could be PUT or PATCH, put for a simple operation like this POST suits better
+        [HttpPost("{id}/setMain")]
+        public async Task<IActionResult> SetMainPhoto(int userId, int id)
+        {
+            if (userId != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+                return Unauthorized();
+
+            var user = await _repo.GetUser(userId);
+
+            if (!user.Photos.Any(p => p.Id == id))
+                return Unauthorized();
+
+            var photoFromRepo = await _repo.GetPhoto(id);
+            if (photoFromRepo.IsMain)
+                return BadRequest("Photo is already set to be the main photo!");
+
+            var currentMainPhoto = await _repo.GetMainPhotoForUser(userId);
+            currentMainPhoto.IsMain = false;
+            photoFromRepo.IsMain = true;
+
+            if (await _repo.SaveAll())
+            {
+                return NoContent();
+            }
+
+            return BadRequest("Could not set main photo");
         }
     }
 }
